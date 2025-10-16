efb1e8995aa12d5f77a3938242e3b078c9922489
Carla <acblanco1991@gmail.com>
2025-10-16 14:59:05 -0400

    subiendo proyecto 1.1

---
+++ frontend/static/javascripts/maps-states-mx.js
@@ -0,0 +1,437 @@
+const mxStates = {
+    "MEX2706": {
+        name: "",
+        list: [
+            {
+                title: "<h2 class='turquesa'>NOROESTE</h2>",
+                items: [
+                    "<h3 class='turquesa'>3</h3>PARQUES INDUSTRIALES",
+                    "<h3 class='turquesa'>899,094.08 m<sup>2</sup></h3>ÁREA BRUTA RENTABLE (GLA m<sup>2</sup>)",
+                    "<h3 class='turquesa'>860,750.96 m<sup>2</sup></h3>ÁREA OCUPADA (m<sup>2</sup>)",
+                    "<h3 class='turquesa'>95.74%</h3>PORCENTAJE DE OCUPACIÓN"
+                ]
+            },
+        ]
+    },
+    "MEX2707": {
+        name: "Baja California Sur",
+        list: [
+            {
+                title: "",
+                items: [
+                    "Empleados locales",
+                    "Proveedores locales",
+                    "Proxima causa para la comunidad"
+                ]
+            },
+        ]
+        
+    },
+    "MEX2708": {
+        name: "",
+        list: [
+            {
+                title: "<h2 class='naranja'>NORESTE</h2>",
+                items: [
+                    "<h3 class='naranja'>10</h3>PARQUES INDUSTRIALES",
+                    "<h3 class='naranja'>1,588,431.66 m<sup>2</sup></h3>ÁREA BRUTA RENTABLE (GLA m<sup>2</sup>)",
+                    "<h3 class='naranja'>1,527,762.73 m<sup>2</sup></h3>ÁREA OCUPADA (m<sup>2</sup>)",
+                    "<h3 class='naranja'>96.18%</h3>PORCENTAJE DE OCUPACIÓN"
+                ]
+            },
+        ]
+    },
+    "MEX2709": {
+        name: "",
+        list: [
+            {
+                title: "<h2 class='turquesa'>NOROESTE</h2>",
+                items: [
+                    "<h3 class='turquesa'>3</h3>PARQUES INDUSTRIALES",
+                    "<h3 class='turquesa'>899,094.08 m<sup>2</sup></h3>ÁREA BRUTA RENTABLE (GLA m<sup>2</sup>)",
+                    "<h3 class='turquesa'>860,750.96 m<sup>2</sup></h3>ÁREA OCUPADA (m<sup>2</sup>)",
+                    "<h3 class='turquesa'>95.74%</h3>PORCENTAJE DE OCUPACIÓN"
+                ]
+            },
+        ]
+    },
+    "MEX2710": {
+        name: "",
+        list: [
+            {
+                title: "<h2 class='turquesa'>NOROESTE</h2>",
+                items: [
+                    "<h3 class='turquesa'>3</h3>PARQUES INDUSTRIALES",
+                    "<h3 class='turquesa'>899,094.08 m<sup>2</sup></h3>ÁREA BRUTA RENTABLE (GLA m<sup>2</sup>)",
+                    "<h3 class='turquesa'>860,750.96 m<sup>2</sup></h3>ÁREA OCUPADA (m<sup>2</sup>)",
+                    "<h3 class='turquesa'>95.74%</h3>PORCENTAJE DE OCUPACIÓN"
+                ]
+            },
+        ]
+    },
+    "MEX2711": {
+        name: "Sinaloa",
+        list: [
+            {
+                title: "",
+                items: [
+                    "2 Centros comerciales",
+                    "72,519 m² de ABR total ",
+                    "67,356 m² de ABR Ocupado",
+                    "93% de ocupación"
+                ]
+            },
+        ]
+    },
+    "MEX2712": {
+        name: "",
+        list: [
+            {
+                title: "<h2 class='turquesa'>NOROESTE</h2>",
+                items: [
+                    "<h3 class='turquesa'>3</h3>PARQUES INDUSTRIALES",
+                    "<h3 class='turquesa'>899,094.08 m<sup>2</sup></h3>ÁREA BRUTA RENTABLE (GLA m<sup>2</sup>)",
+                    "<h3 class='turquesa'>860,750.96 m<sup>2</sup></h3>ÁREA OCUPADA (m<sup>2</sup>)",
+                    "<h3 class='turquesa'>95.74%</h3>PORCENTAJE DE OCUPACIÓN"
+                ]
+            },
+        ]
+    },
+    "MEX2713": {
+        name: "",
+        list: [
+            {
+                title: "<h2 class='turquesa'>NOROESTE</h2>",
+                items: [
+                    "<h3 class='turquesa'>3</h3>PARQUES INDUSTRIALES",
+                    "<h3 class='turquesa'>899,094.08 m<sup>2</sup></h3>ÁREA BRUTA RENTABLE (GLA m<sup>2</sup>)",
+                    "<h3 class='turquesa'>860,750.96 m<sup>2</sup></h3>ÁREA OCUPADA (m<sup>2</sup>)",
+                    "<h3 class='turquesa'>95.74%</h3>PORCENTAJE DE OCUPACIÓN"
+                ]
+            },
+        ]
+    },
+    "MEX2714": {
+        name: "",
+        list: [
+            {
+                title: "<h2 class='naranja'>NORESTE</h2>",
+                items: [
+                    "<h3 class='naranja'>10</h3>PARQUES INDUSTRIALES",
+                    "<h3 class='naranja'>1,588,431.66 m<sup>2</sup></h3>ÁREA BRUTA RENTABLE (GLA m<sup>2</sup>)",
+                    "<h3 class='naranja'>1,527,762.73 m<sup>2</sup></h3>ÁREA OCUPADA (m<sup>2</sup>)",
+                    "<h3 class='naranja'>96.18%</h3>PORCENTAJE DE OCUPACIÓN"
+                ]
+            },
+        ]
+    },
+    "MEX2715": {
+        name: "San Luis Potosí",
+        list: [
+            {
+                title: "",
+                items: [
+                    "1 Centro comercial",
+                    "20,274 m² de ABR total ",
+                    "20,183 m² de ABR Ocupado",
+                    "100% de ocupación"
+                ]
+            },
+        ]
+
+    },
+    "MEX2716": {
+        name: "",
+        list: [
+            {
+                title: "<h2 class='naranja'>NORESTE</h2>",
+                items: [
+                    "<h3 class='naranja'>10</h3>PARQUES INDUSTRIALES",
+                    "<h3 class='naranja'>1,588,431.66 m<sup>2</sup></h3>ÁREA BRUTA RENTABLE (GLA m<sup>2</sup>)",
+                    "<h3 class='naranja'>1,527,762.73 m<sup>2</sup></h3>ÁREA OCUPADA (m<sup>2</sup>)",
+                    "<h3 class='naranja'>96.18%</h3>PORCENTAJE DE OCUPACIÓN"
+                ]
+            },
+        ]
+    },
+    "MEX2717": {
+        name: "",
+        list: [
+            {
+                title: "<h2 class='gris'>BAJÍO Y OCCIDENTE</h2>",
+                items: [
+                    "<h3 class='gris'>6</h3>PARQUES INDUSTRIALES",
+                    "<h3 class='gris'>732,421.95 m<sup>2</sup></h3>ÁREA BRUTA RENTABLE (GLA m<sup>2</sup>)",
+                    "<h3 class='gris'>721,164.89 m<sup>2</sup></h3>ÁREA OCUPADA (m<sup>2</sup>)",
+                    "<h3 class='gris'>98.46%</h3>PORCENTAJE DE OCUPACIÓN"
+                ]
+            },
+        ]
+    },
+    "MEX2718": {
+        name: "Colima",
+        list: [
+            {
+                title: "",
+                items: [
+                    "Empleados locales",
+                    "Proveedores locales",
+                    "Proxima causa para la comunidad"
+                ]
+            },
+        ]
+    },
+    "MEX2719": {
+        name: "",
+        list: [
+            {
+                title: "<h2 class='gris'>BAJÍO Y OCCIDENTE</h2>",
+                items: [
+                    "<h3 class='gris'>6</h3>PARQUES INDUSTRIALES",
+                    "<h3 class='gris'>732,421.95 m<sup>2</sup></h3>ÁREA BRUTA RENTABLE (GLA m<sup>2</sup>)",
+                    "<h3 class='gris'>721,164.89 m<sup>2</sup></h3>ÁREA OCUPADA (m<sup>2</sup>)",
+                    "<h3 class='gris'>98.46%</h3>PORCENTAJE DE OCUPACIÓN"
+                ]
+            },
+        ]
+    },
+    "MEX2720": {
+        name: "Michoacán",
+        list: [
+            {
+                title: "",
+                items: [
+                    "6.0%",
+                    
+                ]
+            },
+        ]
+    },
+    "MEX2721": {
+        name: "Nayarit",
+        list: [
+            {
+                title: "",
+                items: [
+                    "Empleados locales",
+                    "Proveedores locales",
+                    "Proxima causa para la comunidad"
+                ]
+            },
+        ]
+    },
+    "MEX2722": {
+        name: "Campeche",
+        list: [
+            {
+                title: "",
+                items: [
+                    "Empleados locales",
+                    "Proveedores locales",
+                    "Proxima causa para la comunidad"
+                ]
+            },
+        ]
+    },
+    "MEX2723": {
+        name: "Oaxaca",
+        list: [
+            {
+                title: "",
+                items: [
+                    "Empleados locales",
+                    "Proveedores locales",
+                    "Proxima causa para la comunidad"
+                ]
+            },
+        ]
+    },
+    "MEX2724": {
+        name: "",
+        list: [
+            {
+                title: "<h2 class='azulclaro'>CENTRO</h2>",
+                items: [
+                    "<h3 class='azulclaro'>3</h3>PARQUES INDUSTRIALES",
+                    "<h3 class='azulclaro'>197,699.85 m<sup>2</sup></h3>ÁREA BRUTA RENTABLE (GLA m<sup>2</sup>)",
+                    "<h3 class='azulclaro'>183,384.15 m<sup>2</sup></h3>ÁREA OCUPADA (m<sup>2</sup>)",
+                    "<h3 class='azulclaro'>92.76%</h3>PORCENTAJE DE OCUPACIÓN"
+                ]
+            },
+        ]
+    },
+    "MEX2725": {
+        name: "Tabasco",
+        list: [
+            {
+                title: "",
+                items: [
+                    "Empleados locales",
+                    "Proveedores locales",
+                    "Proxima causa para la comunidad"
+                ]
+            },
+        ]
+    },
+    "MEX2726": {
+        name: "Tlaxcala",
+        list: [
+            {
+                title: "",
+                items: [
+                    "Empleados locales",
+                    "Proveedores locales",
+                    "Proxima causa para la comunidad"
+                ]
+            },
+        ]
+    },
+    "MEX2727": {
+        name: "",
+        list: [
+            {
+                title: "<h2 class='azulclaro'>CENTRO</h2>",
+                items: [
+                    "<h3 class='azulclaro'>3</h3>PARQUES INDUSTRIALES",
+                    "<h3 class='azulclaro'>197,699.85 m<sup>2</sup></h3>ÁREA BRUTA RENTABLE (GLA m<sup>2</sup>)",
+                    "<h3 class='azulclaro'>183,384.15 m<sup>2</sup></h3>ÁREA OCUPADA (m<sup>2</sup>)",
+                    "<h3 class='azulclaro'>92.76%</h3>PORCENTAJE DE OCUPACIÓN"
+                ]
+            },
+        ]
+    },
+    "MEX2728": {
+        name: "",
+        list: [
+            {
+                title: "<h2 class='gris'>BAJÍO Y OCCIDENTE</h2>",
+                items: [
+                    "<h3 class='gris'>6</h3>PARQUES INDUSTRIALES",
+                    "<h3 class='gris'>732,421.95 m<sup>2</sup></h3>ÁREA BRUTA RENTABLE (GLA m<sup>2</sup>)",
+                    "<h3 class='gris'>721,164.89 m<sup>2</sup></h3>ÁREA OCUPADA (m<sup>2</sup>)",
+                    "<h3 class='gris'>98.46%</h3>PORCENTAJE DE OCUPACIÓN"
+                ]
+            },
+        ]
+    },
+    "MEX2729": {
+        name: "Guerrero",
+        list: [
+            {
+                title: "",
+                items: [
+                    "Empleados locales",
+                    "Proveedores locales",
+                    "Proxima causa para la comunidad"
+                ]
+            },
+        ]
+    },
+    "MEX2730": {
+        name: "Hidalgo",
+        list: [
+            {
+                title: "",
+                items: [
+                    "Empleados locales",
+                    "Proveedores locales",
+                    "Proxima causa para la comunidad"
+                ]
+            },
+        ]
+    },
+    "MEX2731": {
+        name: "",
+        list: [
+            {
+                title: "<h2 class='azulclaro'>CENTRO</h2>",
+                items: [
+                    "<h3 class='azulclaro'>3</h3>PARQUES INDUSTRIALES",
+                    "<h3 class='azulclaro'>197,699.85 m<sup>2</sup></h3>ÁREA BRUTA RENTABLE (GLA m<sup>2</sup>)",
+                    "<h3 class='azulclaro'>183,384.15 m<sup>2</sup></h3>ÁREA OCUPADA (m<sup>2</sup>)",
+                    "<h3 class='azulclaro'>92.76%</h3>PORCENTAJE DE OCUPACIÓN"
+                ]
+            },
+        ]
+    },
+    "MEX2732": {
+        name: "",
+        list: [
+            {
+                title: "<h2 class='azulclaro'>CENTRO</h2>",
+                items: [
+                    "<h3 class='azulclaro'>3</h3>PARQUES INDUSTRIALES",
+                    "<h3 class='azulclaro'>197,699.85 m<sup>2</sup></h3>ÁREA BRUTA RENTABLE (GLA m<sup>2</sup>)",
+                    "<h3 class='azulclaro'>183,384.15 m<sup>2</sup></h3>ÁREA OCUPADA (m<sup>2</sup>)",
+                    "<h3 class='azulclaro'>92.76%</h3>PORCENTAJE DE OCUPACIÓN"
+                ]
+            },
+        ]
+    },
+    "MEX2733": {
+        name: "",
+        list: [
+            {
+                title: "<h2 class='gris'>BAJÍO Y OCCIDENTE</h2>",
+                items: [
+                    "<h3 class='gris'>6</h3>PARQUES INDUSTRIALES",
+                    "<h3 class='gris'>732,421.95 m<sup>2</sup></h3>ÁREA BRUTA RENTABLE (GLA m<sup>2</sup>)",
+                    "<h3 class='gris'>721,164.89 m<sup>2</sup></h3>ÁREA OCUPADA (m<sup>2</sup>)",
+                    "<h3 class='gris'>98.46%</h3>PORCENTAJE DE OCUPACIÓN"
+                ]
+            },
+        ]
+    },
+    "MEX2734": {
+        name: "Veracruz",
+        list: [
+            {
+                title: "",
+                items: [
+                    "Empleados locales",
+                    "Proveedores locales",
+                    "Proxima causa para la comunidad"
+                ]
+            },
+        ]
+    },
+    "MEX2735": {
+        name: "Chiapas",
+        list: [
+            {
+                title: "",
+                items: [
+                    "Empleados locales",
+                    "Proveedores locales",
+                    "Proxima causa para la comunidad"
+                ]
+            },
+        ]
+    },
+    "MEX2736": {
+        name: "Quintana Roo",
+        list: [
+            {
+                title: "",
+                items: [
+                    "Empleados locales",
+                    "Proveedores locales",
+                    "Proxima causa para la comunidad"
+                ]
+            },
+        ]
+    },
+    "MEX2737": {
+        name: "Yucatán - Mérida",
+        list: [
+            {
+                title: "",
+                items: [
+                    "Empleados locales",
+                    "Proveedores locales",
+                    "Proxima causa para la comunidad"
+                ]
+            },
+        ]
+    }
+}
